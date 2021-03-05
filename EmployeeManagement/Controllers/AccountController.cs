ae592b7643657c55dedc24ea42ebea340e812d9a
Siddharth Jaiswal <Siddharth.Jaiswal@blueyonder.com>
2021-03-05 15:39:43 +0530

    Implementing login functionality in asp net core

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -41,14 +41,38 @@ namespace EmployeeManagement.Controllers
 
         if (result.Succeeded)
         {
+          // Keep the logged in user information in the session memory using (isPersistent: false)
           await signInManager.SignInAsync(user, isPersistent: false);
           return RedirectToAction("Index", "home");
         }
 
         foreach (var error in result.Errors)
         {
-          ModelState.AddModelError("", error.Description);
+          ModelState.AddModelError(string.Empty, error.Description);
         }
+      }
+
+      return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+      return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+      if (ModelState.IsValid)
+      {
+        var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index", "home");
+        }
+
+          ModelState.AddModelError(string.Empty, "Invalid Username or Password");
       }
 
       return View(model);
