29d3aa50db27b1d246cbe680844095c2d6e5f544
Siddharth Jaiswal <Siddharth.Jaiswal@blueyonder.com>
2021-03-07 01:11:41 +0530

    Get list of roles in asp net core

--- EmployeeManagement/Controllers/AdminController.cs
+++ EmployeeManagement/Controllers/AdminController.cs
@@ -37,7 +37,7 @@ namespace EmployeeManagement.Controllers
 
         if (result.Succeeded)
         {
-          return RedirectToAction("Index", "home");
+          return RedirectToAction("ListRoles", "Admin");
         }
 
         foreach (var error in result.Errors)
@@ -46,6 +46,13 @@ namespace EmployeeManagement.Controllers
         }
       }
       return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult ListRoles()
+    {
+      var roles = roleManager.Roles;
+      return View(roles);
     }
   }
 }
